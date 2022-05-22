fa4be96549aa8ff1fea4f120043704881cccef2e
Matt Wong <wongmatthew357@gmail.com>
2022-05-22 00:18:51 -0700

    CSS Changes
    Last commit for the night, just played around with css

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -162,58 +162,6 @@ const App = () => {
       })}
 
     </div>
-
-    
-//     <div className="App">
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           <input
-//             type="text"
-//             onChange={handleChange}
-//             placeholder="enter pokemon name"
-//           />
-//         </label>
-//       </form>
-//       {/* <ul>{pokemonData}</ul> */}
-//       {/* <p>{[pokemonData]}</p> */}
-//       {pokemonData.map((data) => {
-//         return (
-//           <div className="container">
-//             <img src={data.sprites["front_default"]} />
-//             <div className="divTable">
-//               <div className="divTableBody">
-//                 <div className="divTableRow">
-//                   <div className="divTableCell">Type</div>
-//                   <div className="divTableCell">{pokemonType}</div>
-//                 </div>
-//                 <div className="divTableRow">
-//                   <div className="divTableCell">Height</div>
-//                   <div className="divTableCell">
-//                     {" "}
-//                     {Math.round(data.height * 3.9)}"
-//                   </div>
-//                 </div>
-//                 <div className="divTableRow">
-//                   <div className="divTableCell">Weight</div>
-//                   <div className="divTableCell">
-//                     {" "}
-//                     {Math.round(data.weight / 4.3)} lbs
-//                   </div>
-//                 </div>
-//                 <div className="divTableRow">
-//                   <div className="divTableCell">Abilities</div>
-//                   <div className="divTableCell">{data.abilities}</div>
-//                 </div>
-//                 <div className="divTableRow">
-//                   <div className="divTableCell">Base Stats</div>
-//                   <div className="divTableCell">{data.game_indices.length}</div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
    );
 };
 
